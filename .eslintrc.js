2ce5d26f453b2e68881d3902ea83b93cee1746db
Christopher Goh <chrisgzf@gmail.com>
2020-06-20 00:04:03 +0800

    Add eslint jest plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,19 @@ module.exports = {
     commonjs: true,
     es2020: true,
     node: true,
+    'jest/globals': true,
   },
   // Ensure `prettier` is the last in `extends`
   extends: ['airbnb-base', 'prettier/react', 'prettier'],
   parserOptions: {
     ecmaVersion: 11,
   },
-  rules: {},
+  plugins: ['jest'],
+  rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  },
 };
